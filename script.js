2f0dad33b097d62a3e87c66e61849e87402f25ea
Jonathan Fuentes <john.fuen95@gmail.com>
2023-11-13 21:51:50 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector('.btn');
+const coupon = document.querySelector('.coupon');
+
+btn.addEventListener('click', copyText);
+
+function copyText(e) {
+    e.preventDefault();
+
+    navigator.clipboard.writeText(coupon.value)
+        .then(() => {
+            btn.textContent = 'Copied!';
+
+            setTimeout(() => {
+                btn.textContent = 'Copy';
+            }, 2000)
+        })
+};
